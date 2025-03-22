5889e0ecf15495663f4f818ef3468f6278662add
happygetgit <sk39happy@gmail.com>
2025-03-23 00:41:31 +0530

    taken api from newsapi website, added in sampleout json file, and structure of the newsitem component is given

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,19 @@ import NewsItem from "./NewsItem";
 export default class News extends Component {
   render() {
     return (
-      <div>
-        <h1> This is a News Component</h1>
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-3">
+        <h2> News App - Top Headlines </h2>
+        <div className="row">
+          <div className="col-md-3">
+            <NewsItem title="MyTitle" description="my description" />
+          </div>
+          <div className="col-md-3">
+            <NewsItem title="MyTitle" description="my description" />
+          </div>
+          <div className="col-md-3">
+            <NewsItem title="MyTitle" description="my description" />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    return <div>This is a news item.</div>;
+    let { title, description } = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://variety.com/wp-content/uploads/2025/03/MCDSNWH_WD029.jpg?w=1000&h=563&crop=1"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
